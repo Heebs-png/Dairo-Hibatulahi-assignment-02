d671f5a36a68ad408f472153dabf153f258e1acf
Dairo Hibatulahi Kehinde <dairohibat@gmail.com>
2022-07-13 10:18:27 +0000

    Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -1,23 +1,26 @@
 // ========================
 // DO NOT EDIT THIS BLOCK
 const assignment = {};
-// ========================
-
-
+// =================
 /**
  * Challenge - 1
  * 
  * Calculate and return the sum of the numbers in an array.
  * If you did Challenge - 1, remove the comment in the line just after this function
  * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
+*@param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum =0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i];
+    }
+      return sum;
+      
 }
-// assignment.sumOfNumbers = sumOfNumbers;
-
+console.log(sumOfNumbers([2, 4, 6, 8, 10]));
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -28,10 +31,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+  for(let i =0; i < arrayOfNumbers.length; i++){
+    if(arrayOfNumbers[i] % 2 === 0){
+      count++
+      }  
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
-
+console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+assignment.countEvenNumbers = countEvenNumbers;
 /**
  * Challenge - 3
  * 
